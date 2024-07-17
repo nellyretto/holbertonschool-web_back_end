9037eafc509c83cfda6a2a60883107a130cf3848
nellyretto <danielys2416@gmail.com>
2024-07-17 13:38:28 -0400

    ```text refactor: Update getIncomeInEuros method to accept income parameter
    This commit updates the getIncomeInEuros method in the getFullBudgetObject function to accept an income parameter. Previously, the method did not take any arguments. This change allows for more flexibility in calculating the income in euros based on different income values.

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -7,7 +7,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros() {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
