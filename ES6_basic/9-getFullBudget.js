c51997f1736f0cf014b668cf1a6607cb52d48d14
nellyretto <danielys2416@gmail.com>
2024-07-17 13:36:38 -0400

    Update getIncomeInDollars method to use arrow function

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -4,7 +4,9 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: () => `$${income}`,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
     getIncomeInEuros() {
       return `${income} euros`;
     },
