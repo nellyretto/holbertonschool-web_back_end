af4fc4307bd4cb975f8e5e5b734b44e3d358c7ed
nellyretto <danielys2416@gmail.com>
2024-07-19 20:30:40 -0400

    refactor: Update Currency class with getters and setters
    This commit updates the Currency class by adding getters and setters for the name and code properties. This allows for better encapsulation and control over the class's internal state. Additionally, the constructor now includes type checking to ensure that the code and name parameters are both strings. This improves the reliability and maintainability of the code.

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,31 @@
+/* eslint no-underscore-dangle: 0 */
 export default class Currency {
-    constructor (code, name) {
-        this._code = code
-        this._name= name;
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') {
+      throw TypeError('Must be a sting');
     }
 
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
