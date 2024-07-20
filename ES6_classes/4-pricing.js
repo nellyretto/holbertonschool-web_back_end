a0f4bb82355160fe64cafbaee9141f0a594fc7b6
nellyretto <danielys2416@gmail.com>
2024-07-19 21:35:38 -0400

    “Adding and testing functionality for task 4”

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+/* eslint no-underscore-dangle: 0 */
+export default class Pricinfg {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} (${this._currency.displayFullCurrency()})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
