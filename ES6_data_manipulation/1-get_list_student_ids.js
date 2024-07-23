eb41846ee08101c166b46c1364e9cc6f400c4caa
nellyretto <danielys2416@gmail.com>
2024-07-23 18:48:47 -0400

    changing task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
+export default function getListStudentIds(list) {
+  if (!Array.isArray(list)) {
     return [];
   }
-  return students.map((student) => student.id);
+  return list.map((student) => student.id);
 }
