27e647f7b696b3ce557a56260ba1e8a4eddcd28f
Nelly <danielys2416@gmail.com>
2024-07-23 20:53:23 -0400

    Delete ES6_data_manipulation/8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++
@@ -1,7 +0,0 @@
-export default function cleanSet(Set, startString) {
-  const cleanedValues = Array.from(Set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
-  return cleanedValues;
-}
