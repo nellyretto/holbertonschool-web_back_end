a43513c79701a3d38487872f1fbaf3334383d031
nellyretto <danielys2416@gmail.com>
2024-07-24 20:03:49 -0400

    Adding task 0 and testing functionality

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
+// el error es cuando corre eslint ? si si
+// ok, deja ver que puedo hacer we
+// ya esta working dama
+// happy coding !!
+// hahah no hay de que we XD
