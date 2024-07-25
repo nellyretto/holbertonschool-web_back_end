cd206c69237604f8eb8dfd37a94dd68f8dbf0d15
nellyretto <danielys2416@gmail.com>
2024-07-25 00:07:07 -0400

    feat: Add ES6 promise implementation and test functionality

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,9 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      const { firstName, lastName } = user;
-      console.log(`${photo} ${firstName} ${lastName}`);
+    .then((output) => {
+      const [photo, user] = output;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
