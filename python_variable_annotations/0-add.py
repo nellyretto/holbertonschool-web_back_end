bdd80522c24f11a5e5060ac736dff9eaab4b7e36
nellyretto <danielys2416@gmail.com>
2024-08-13 14:39:16 -0400

    adding first task

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+0. Basic annotations - add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two numbers and return the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
