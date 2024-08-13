32924b85b5f11fda9edf190ed55656cb8358aa9a
nellyretto <danielys2416@gmail.com>
2024-08-13 17:15:59 -0400

    adding task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This file defines variables with specific types.
+
+Variables:
+- a: int - an integer variable with a value of 1.
+- pi: float - a floating-point variable with a value of 3.14.
+- i_understand_annotations: bool - a boolean variable with a value of True.
+- school: str - a string variable with a value of "Holberton".
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
