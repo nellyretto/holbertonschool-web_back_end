48dddcb4bbacd1c3c4f2fc3d864f1cf06bf81e37
nellyretto <danielys2416@gmail.com>
2024-08-13 14:57:32 -0400

    adding task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,22 @@
+#!/usr//bin/env python3
+"""
+1. Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string to be concatenated.
+        str2 (str): The second string to be concatenated.
+
+    Returns:
+        str: The concatenated string.
+
+    Example:
+        concat("Hello", "World")
+        'HelloWorld'
+    """
+    return str1 + str2
