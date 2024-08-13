ca4c0baf0da445acca6c1b0802a051da907bca60
nellyretto <danielys2416@gmail.com>
2024-08-13 16:10:50 -0400

    adding task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import math
+"""
+2. Basic annotations - floor
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Returns the largest integer less than or equal to a given number.
+
+    Args:
+        n (float): The number to be rounded down.
+
+    Returns:
+        float: The largest integer less than or equal to `n`.
+    """
+    return math.floor(n)
