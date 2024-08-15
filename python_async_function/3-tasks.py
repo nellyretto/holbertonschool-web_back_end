36a0a2cba81b9b5480e71990ad6d78169ebb3238
nellyretto <danielys2416@gmail.com>
2024-08-15 19:51:28 -0400

    adding and testing functionality for task 3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+"""
+Imports the `wait_random` function from the module '0-basic_async_syntax'.
+This function is assumed to be defined in a separate file and
+returns a random delay.
+"""
+
+
+def task_wait_random(max_delay: int):
+    """
+    Defines an asynchronous task that calls the `wait_random` function.
+
+    Args:
+        max_delay (int): The maximum delay in seconds for
+        the `wait_random` function.
+
+    Returns:
+        asyncio.Task: An asyncio Task object representing the
+        \asynchronous operation.
+
+    This function creates an asyncio Task object that executes
+    the `wait_random` function
+    with the given `max_delay`. The `wait_random` function is
+    expected to return a random
+    delay within the specified maximum.
+    """
+    return asyncio.Task(wait_random(max_delay))
