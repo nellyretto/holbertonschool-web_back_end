0c17bd6023f48b6c3dcbc29be78004d36dcae1cf
nellyretto <danielys2416@gmail.com>
2024-08-20 18:19:56 -0400

    updating task 01

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
 
     Returns:
