61c111c4c6770cbe9da9ab77b8f2fb4d695ec1c4
nellyretto <danielys2416@gmail.com>
2024-08-20 16:32:06 -0400

    adding task 01

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Import libraries for asynchronous
+programming, random number generation, and type hinting
+"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+
+    Returns:
+        AsyncGenerator[float, None]: _description_
+
+    Yields:
+        Iterator[AsyncGenerator[float, None]]: _description_
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
