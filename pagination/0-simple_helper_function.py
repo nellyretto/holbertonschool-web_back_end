96aa49930983fafcc3873f68e763b4e56c6aa3b9
nellyretto <danielys2416@gmail.com>
2024-08-21 14:28:26 -0400

    "Implement and test task 00 functionality"

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def index_range(page, page_size) -> tuple:
+    """
+    Returns the start and end index of a given page in a pagination system.
+
+    Args:
+        page (int): The page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end index of the page.
+
+    Example:
+        >>> index_range(1, 10)
+        (0, 10)
+        >>> index_range(2, 10)
+        (10, 20)
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
