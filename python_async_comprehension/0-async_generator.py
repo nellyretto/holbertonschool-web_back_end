222030ebe436f12dcab19af479077b864661c452
nellyretto <danielys2416@gmail.com>
2024-08-20 22:27:42 -0400

    changing task 00

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ programming, random number generation, and type hinting
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
 
     Returns:
