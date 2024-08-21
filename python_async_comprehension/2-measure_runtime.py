a50be6a8e524b223d28d5b1ee3e2b4273bd7f275
nellyretto <danielys2416@gmail.com>
2024-08-20 22:38:53 -0400

    checking updates on task 02 syntax

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -15,9 +15,9 @@ Function:
     taken to run async_comprehension four times concurrently.
 """
 
+
 import asyncio
 import time
-
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
