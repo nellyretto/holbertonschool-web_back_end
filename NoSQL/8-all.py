972fd757faa7bf5efeacc563eca7189b55ea5e8b
nellyretto <danielys2416@gmail.com>
2024-08-22 14:17:28 -0400

    updating task 08 and testing

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-
+"""import pymongo"""
 import pymongo
 
+
 def list_all(mongo_collection):
-    
-    return list (mongo_collection.find())
+    """_summary_
+
+    Args:
+        mongo_collection (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return list(mongo_collection.find())
