64796d61ac54b5f3aaaf2c5d7853f9fbb147a4f8
nellyretto <danielys2416@gmail.com>
2024-09-26 19:18:14 -0400

    fixing eslint coprrection

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1); // Skip the header line
 
     if (students.length === 0) {
